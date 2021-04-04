118025b26c1d97661ae4bb804945f0809e48b70e
yl1462 <yliang1462@hotmail.com>
2021-04-04 12:27:16 -0500

    updated api_endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
 export default {
   // API_ENDPOINT: 'http://localhost:8000/api'
-  API_ENDPOINT: 'https://todolist-app-1lxb17p1b-yl1462.vercel.app/'
+  API_ENDPOINT: 'https://afternoon-springs-29511.herokuapp.com/'
 }
